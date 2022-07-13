c8e1e0f62b3ddbf1148e9cdf24374414ed7dbc6d
mltlima <miguellt@dcc.ufrj.br>
2022-07-13 17:25:02 -0300

    error and auth  middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export async function validateToken(req: Request, res: Response, next: NextFunction) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "").trim();
+  const secretKey = process.env.JWT_TOKEN;
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+   
+  const user = jwt.verify(token, secretKey!);
+  if (!user) {
+    return res.status(401);
+  }
+  res.locals.user = user;
+  next();
+  
+}
